0150adffa9d482f988acd0160aec84b80511de62
Jeremy VAILLANT <jeremy@network.fr>
2016-04-20 11:56:47 +0200

    Fix hash reinit value to false

--- lib/phpcop/runner.rb
+++ lib/phpcop/runner.rb
@@ -6,10 +6,12 @@ module PhpCop
 
     attr_reader :count_files, :count_errors
 
-    def initialize(rules)
+    def initialize(config_store)
       @count_files = 0
       @count_errors = 0
-      @rules = rules
+      @conf = config_store
+      @rules = @conf.rules
+      @types = { files: false }
 
       Dir.foreach(Dir.pwd) do |file|
         run_folder(file, Dir.pwd)
@@ -38,32 +40,39 @@ module PhpCop
     # Test each rules
     def execute_tests_in_file(file, path)
       @count_files += 1
+      file_with_path = format('%s/%s', path, file)
+      types_default
       @rules.each do |value|
         case value.type
         when 'files'
-          test_file(file, path)
+          test_file(file_with_path, value.type) unless @types['files']
         end
       end
     end
 
-    def test_file(file, path)
-      @rules.each do |value|
+    def test_file(file, type)
+      @conf.rules_by_type(type).each do |value|
         case value.name
         when 'phpTags'
-          test_file_php_tags(file, path) if value.enabled
+          test_file_php_tags(file) if value.enabled
         when 'phpEncoding'
           test_file_php_encoding if value.enabled
         end
       end
+      @types['files'] = true
     end
 
-    def test_file_php_tags(file, path)
-      test = PhpCop::Cop::Files::PhpTags.new(format('%s/%s', path, file))
+    def test_file_php_tags(file)
+      test = PhpCop::Cop::Files::PhpTags.new(file)
       @count_errors += test.errors
     end
 
     def test_file_php_encoding
       true
+    end
+
+    def types_default
+      @types.map { |k, _| @types[k] = false }
     end
   end
 end
