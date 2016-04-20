2c2d510eb5666b13c0181e5ecd7ff1de732d587a
Jeremy VAILLANT <jeremy@network.fr>
2016-04-20 10:23:37 +0200

    Add rule to project

--- lib/phpcop.rb
+++ lib/phpcop.rb
@@ -4,6 +4,7 @@ require 'phpcop/configloader'
 require 'phpcop/configstore'
 require 'phpcop/cli'
 require 'phpcop/runner'
+require 'phpcop/rule'
 
 require 'phpcop/cop/files/phptags.rb'
 require 'phpcop/cop/files/phpencoding.rb'
