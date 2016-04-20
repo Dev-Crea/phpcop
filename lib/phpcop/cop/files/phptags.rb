6a12c15a4d5977c583d5443e115a6de70ecf752b
Jeremy VAILLANT <jeremy@network.fr>
2016-04-20 17:28:26 +0200

    Fix encode line for searching php tags

--- lib/phpcop/cop/files/phptags.rb
+++ lib/phpcop/cop/files/phptags.rb
@@ -25,24 +25,30 @@ module PhpCop
           @line = line
           @line_number = line_number
           # Parse file and search tags exists
-          parse_and_search_open_tag(@line)
+          parse_and_search_open_tag
           # If file contains tag <?php or <?= search close tags
-          parse_and_search_close_tag(@line)
+          parse_and_search_close_tag
         end
 
         private
 
         # Parse file and search tags
-        def parse_and_search_open_tag(line)
+        def parse_and_search_open_tag
           TAG_OPEN.each do |rule|
-            @count_open += 1 if Regexp.new(rule).match line
+            @count_open += 1 if match_line(rule)
           end
         end
 
-        def parse_and_search_close_tag(line)
+        def parse_and_search_close_tag
           TAG_CLOSE.each do |rule|
-            @count_close += 1 if Regexp.new(rule).match line
+            @count_close += 1 if match_line(rule)
           end
+        end
+
+        def match_line(rule)
+          Regexp.new(rule).match @line.encode('UTF-16be',
+                                              invalid: :replace,
+                                              replace: '?').encode('UTF-8')
         end
 
         def return_an_error(file, line, column)
