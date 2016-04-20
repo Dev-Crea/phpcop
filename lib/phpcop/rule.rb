8fe59b1bc0ac32139447c5705c497812126824ed
Jeremy VAILLANT <jeremy@network.fr>
2016-04-20 10:10:12 +0200

    Add type

--- lib/phpcop/rule.rb
+++ lib/phpcop/rule.rb
@@ -1,10 +1,12 @@
 module PhpCop
   # Object represent a rule in yaml configuration file
   class Rule
-    attr_reader :name, :enabled, :description, :see
+    attr_reader :name, :type, :enabled, :description, :see
 
     def initialize(name, option = {})
-      @name = name
+      nt = name.split('/')
+      @name = nt[1]
+      @type = nt[0]
       @enabled = option.enabled
       @description = option.description
       @see = option.see
