0bbaa0abbb26559761e617656c48962c40f13710
Jeremy VAILLANT <jeremy@network.fr>
2016-04-20 09:08:02 +0200

    Add test class encoding utf8

--- lib/phpcop.rb
+++ lib/phpcop.rb
@@ -6,3 +6,4 @@ require 'phpcop/cli'
 require 'phpcop/runner'
 
 require 'phpcop/cop/files/phptags.rb'
+require 'phpcop/cop/files/phpencoding.rb'
---
+++ lib/phpcop/cop/files/phpencoding.rb
@@ -0,0 +1,10 @@
+module PhpCop
+  module Cop
+    module Files
+      # This class test file php use correctly encoding type
+      class PhpEncoding
+        MSG_ALERT = '%s, is not encoding in UTF-8 without BOM.'.freeze
+      end
+    end
+  end
+end
