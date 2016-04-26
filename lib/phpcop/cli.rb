987c8b70a6ecdaf1684c04180a35efc9d8199d57
Jeremy VAILLANT <jeremy@network.fr>
2016-04-26 16:04:39 +0200

    Add space to result if number of file and error is big

--- lib/phpcop/cli.rb
+++ lib/phpcop/cli.rb
@@ -7,7 +7,7 @@ module PhpCop
   class CLI
     attr_reader :config_store
 
-    MSG_END = '%s fichier traité. %s erreurs.'
+    MSG_END = '%s fichiers traités. %s erreurs.'
 
     def initialize
       @config_store = ConfigStore.new
@@ -16,7 +16,27 @@ module PhpCop
     # Run all files
     def run(_args = ARGV)
       runner = PhpCop::Runner.new(@config_store)
-      puts format(MSG_END, runner.count_files, runner.count_errors)
+      puts format(MSG_END,
+                  format_count(runner.count_files),
+                  format_count(runner.count_errors))
+    end
+
+    private
+
+    def format_count(count)
+      i = 0
+      f = []
+      count.to_s.reverse.split('').each do |letter|
+        i += 1
+        f.push(letter)
+        i = add_space(f) if i == 3
+      end
+      f.join.to_s.reverse
+    end
+
+    def add_space(f)
+      f.push(' ')
+      0
     end
   end
 end
